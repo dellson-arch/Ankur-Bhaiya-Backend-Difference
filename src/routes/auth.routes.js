9e4517803112d6315954d6cd885512a3399e7e96
Nayan Mahato <nayanmahato904@gmail.com>
2026-05-25 07:12:49 +0530

    Commited my first googleOAuth using passport

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,4 +1,19 @@
 const express = require('express')
+const passport = require('passport')
+let jwt = require("jsonwebtoken")
 let router = express.Router()
 
-router.post('/register' , )
+// router.post('/register' , )
+
+router.get('google' , passport.authenticate("google" , {scope : ["profile" , "email"], session:false}))
+router.get('/google/callback', passport.authenticate("google" , {failureRedirect:'/',session : false}), (req,res)=>{
+    let token = jwt.sign({id : req.user._id} , process.env.JWT_SECRET_ACCESS,{
+        expiresIn : "15m"
+    })
+    res.cookie("token", token)
+
+    return res.send("ok")
+})
+
+
+module.exports = router
