de150311e6356cabfeb66cc16182a27267e6e51c
Nayan Mahato <nayanmahato904@gmail.com>
2026-06-04 11:37:28 +0530

    path fixed of register and forget

--- forgetPassword/src/app.js
+++ forgetPassword/src/app.js
@@ -1,8 +1,12 @@
 let express = require('express')
 let cookieParser = require("cookie-parser");
+const authroutes = require('../src/routes/auth.routes')
+
 let app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
+app.use('/api/auth' , authroutes)
 
 module.exports = app
